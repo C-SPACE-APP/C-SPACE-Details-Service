5358201928b20aca2d2477c49426c1865a061d35
Junel Alje Isanan <jbisanan@up.edu.ph>
2022-07-01 22:33:40 +0800

    add comment database and schema

--- utils/initializeDatabase.js
+++ utils/initializeDatabase.js
@@ -5,18 +5,24 @@ const initializeDatabase = async () =>
     let isSuccessful = false
     const createPostDatabaseQuery = "CREATE DATABASE IF NOT EXISTS PostService;"
     const createPostTagDatabaseQuery = "CREATE DATABASE IF NOT EXISTS PostTagService;"
-    const createPostTagTableQuery = "CREATE TABLE IF NOT EXISTS PostTag.PostTag(postID INT, tagName TEXT);"
+    const createCommentDatabaseQuery = "CREATE DATABASE IF NOT EXISTS CommentService;"
+
+    const createPostTagTableQuery = "CREATE TABLE IF NOT EXISTS PostTagService.PostTag(postID INT, tagName TEXT);"
     const createPostTableQuery = "CREATE TABLE IF NOT EXISTS PostService.Post(postID INT PRIMARY KEY AUTO_INCREMENT, title TEXT, description TEXT, createdAt DATETIME, updatedAt DATETIME);"
+    const createCommentTableQuery = "CREATE TABLE IF NOT EXISTS CommentService.Comment(commentID INT PRIMARY KEY AUTO_INCREMENT, comment TEXT, createdAt DATETIME, updatedAt DATETIME);"
+    
     let connection
     try
     {
         connection = await establishConnection(false)
         await connection.execute(createPostDatabaseQuery)
         await connection.execute(createPostTagDatabaseQuery)
+        await connection.execute(createCommentDatabaseQuery)
         await connection.destroy()
         connection = await establishConnection(false)
         await connection.execute(createPostTableQuery)
         await connection.execute(createPostTagTableQuery)
+        await connection.execute(createCommentTableQuery)
         isSuccessful = true
         console.log("Database initialization successful")
     }
